ee14f1b5af885bfcc0786743122b5200b6db3b54
thefirstdesginer <thefirstdesginer@gmail.com>
2025-05-09 18:56:33 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+
+  const backToTopBtn = document.getElementById("backToTop");
+
+  window.addEventListener("scroll", () => {
+    backToTopBtn.style.display = window.scrollY > 300 ? "block" : "none";
+  });
+
+  backToTopBtn.addEventListener("click", () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
